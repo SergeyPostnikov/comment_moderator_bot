a9b9116fa3d2d88990970d4584f602ae7fc087de
Sergey Postnikov <ser.post2017@gmail.com>
2023-11-21 18:46:12 +0300

    add buttons

--- comment_moderator/bot/signals.py
+++ comment_moderator/bot/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
-from telegram import Bot
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup
 from .models import Comment
 
 TOKEN = settings.TG_KEY
@@ -12,8 +12,18 @@ bot = Bot(token=TOKEN)
 def send_comment_notification(sender, instance, **kwargs):
     chat_id = settings.ADMIN_ID
     message_text = f"Новый комментарий:\n\n{instance.text}"
-
+    
+    keyboard = [
+        [InlineKeyboardButton("Опубликовать", callback_data='publish'),
+         InlineKeyboardButton("Отклонить", callback_data='reject')],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
     try:
-        bot.send_message(chat_id=chat_id, text=message_text)
+        bot.send_message(
+            chat_id=chat_id, 
+            text=message_text,
+            reply_markup=reply_markup
+        )
     except Exception as e:
         print(f"Ошибка при отправке сообщения: {e}")
